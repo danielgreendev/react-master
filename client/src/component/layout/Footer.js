2ada64d704704e6197dd3ddceddeba7a2ab5edfb
danielgreendev <danielgreendev@gmail.com>
2023-04-30 11:06:54 -0400

    upd: upd one more

--- client/src/component/layout/Footer.js
+++ client/src/component/layout/Footer.js
@@ -3,6 +3,9 @@ import React from 'react'
 function Footer() {
     return (
         <footer className='footer fixed-bottom footer-light bg-white p-3 text-center'>
+            <h1 className='display-1 text-black'>
+                <i className='fab fa-telegram-plane'></i>
+            </h1>
             Copyright @ My Git Project
         </footer>
     )
